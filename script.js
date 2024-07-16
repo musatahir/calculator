2b50b7e6370545f921e459d36f8c955ccaee2dea
Musa Tahir <musaasimtahir@gmail.com>
2024-07-16 00:54:15 -0700

    feat: implement number and operator logic

--- script.js
+++ script.js
@@ -10,9 +10,10 @@ const divide = (a, b) => {
     return a / b;
 }
 
-let first;
-let second;
-let op;
+const isValid = (a) => {
+    const digitPattern = /\d/;
+    return digitPattern.test(a);
+}
 
 function operator(operator, a, b) {
     switch (operator) {
@@ -28,6 +29,94 @@ function operator(operator, a, b) {
             throw new Error("Unsupported Operator");
     }
 }
+
+
+
+let firstNumber = "";
+let secondNumber = "";
+let operation = null;
+let displayValue = "0";
+
+const numberButtons = document.querySelectorAll(".button-number");
+const operationButtons = document.querySelectorAll(".button-operator");
+const percentButton = document.querySelector(".percent");
+const signButton = document.querySelector(".sign");
+const decimalButton = document.querySelector(".decimal");
+const equalsButton = document.querySelector(".equals");
+const clearButton = document.querySelector(".clear");
+
+function trimDisplayValue() {
+    const MAX_LENGTH = 29;
+    if (displayValue.length > MAX_LENGTH) {
+        displayValue = displayValue.slice(-MAX_LENGTH);
+    }
+}
+
+function updateDisplay() {
+    trimDisplayValue()
+    const displayElement = document.querySelector('.display-input');
+    displayElement.innerText = displayValue;
+}
+
+
+let freeOperator = false;
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const digit = button.innerHTML;
+        if (operation === null) {
+            if (displayValue === "0") {
+                firstNumber = digit;
+                displayValue = digit;
+            } else {
+                firstNumber += digit;
+                displayValue += digit;
+            }
+
+            if (isValid(firstNumber)) {
+                freeOperator = true;
+            }
+        } else {
+
+            if (displayValue === firstNumber) {
+                secondNumber = digit;
+                displayValue = digit;
+            } else {
+                secondNumber += digit;
+                displayValue += digit;
+            }
+
+            if (isValid(secondNumber)) {
+                freeOperator = true;
+            }
+        }
+
+        updateDisplay();
+    })
+})
+
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const buttonOperation = button.innerHTML;
+        if (operation === null) {
+            if (freeOperator === true) {
+                firstNumber = displayValue;
+                operation = buttonOperation;
+                freeOperator = false;
+            }
+        }
+        else if (freeOperator === true) {
+            displayValue = String(operator(operation, +firstNumber, +displayValue));
+            updateDisplay()
+            firstNumber = displayValue;
+            operation = buttonOperation;
+            secondNumber = "";
+            freeOperator = false;
+        }
+
+    })
+})
 
 
 
